81c4fbd4a3928d51b30db6887ef88dd90c2ec0ef
Szymon Forst <szymonforstbiznes@gmail.com>
2022-07-02 19:00:14 +0200

    formatted buttons code

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,9 +2,11 @@ import "./style.css"
 const Buttons = (props) => 
  ( props.tasks.length > 0 && (
   <div className="allTasksButtons"> 
-      <button className={`list__allTaskButton `}>{props.hideTaskButtons ? "pokaż ukończone" : "urkyj ukończone"}</button>
+      <button className={`list__allTaskButton `}>
+        {props.hideTaskButtons ? "pokaż ukończone" : "urkyj ukończone"}
+      </button>
       <button className="list__allTaskButton"
-      disabled={props.tasks.every(({ done }) => done)}>
+        disabled={props.tasks.every(({ done }) => done)}>
         ukończ wszystkie
        </button>
     </div>
